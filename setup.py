330933cfe91e1df06d7281a78f604f94dee9a9af
galshi <59773096+galshi@users.noreply.github.com>
2022-02-15 16:48:32 +0200

    Fixed typo in repo name

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A tool that helps you to test only those kustomize overlays that were affected by a merge in your CI.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/galshi/get-chaged-kustomize-overlays",
+    url="https://github.com/galshi/get-changed-kustomize-overlays",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
